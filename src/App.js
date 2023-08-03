cde63b85d0cd268aa0675ee63a93ff6bc59ff867
Marek Małachowski <marekmalach11@gmail.com>
2023-08-03 20:31:11 +0200

    Navbar implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>TatarRooms</h1>
+      <Navbar />
     </div>
   );
 }
